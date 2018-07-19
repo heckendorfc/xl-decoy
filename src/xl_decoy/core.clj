16916b013134f8854ccb8384644e232b2532c4e3
Christian Heckendorf <heckendorfc@gmail.com>
2018-07-19 11:52:49 -0400

    export (savedecoy)

--- src/xl_decoy/core.clj
+++ src/xl_decoy/core.clj
@@ -1,5 +1,5 @@
 (ns xl-decoy.core
-  (:require [xl-decoy.decoy :refer [makedecoy]]))
+  (:require [xl-decoy.decoy :refer [makedecoy savedecoy]]))
 
 ;(defn -main
   ;"I don't do a whole lot ... yet."
